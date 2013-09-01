37447a00901e83eff7c552cd8c25ebb0e3c078cd
Mateus Revoredo <mateus.revoredo@gmail.com>
2013-09-01 18:49:22 -0300

    Default Controller and route Test

--- Trunk/Src/Fusiones.Main/Fusiones.Web/Controllers/HomeController.cs
+++ Trunk/Src/Fusiones.Main/Fusiones.Web/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace Fusiones.Web.Controllers
 {
-    public class Home : Controller
+    public class HomeController : Controller
     {
         //
         // GET: /Home/
