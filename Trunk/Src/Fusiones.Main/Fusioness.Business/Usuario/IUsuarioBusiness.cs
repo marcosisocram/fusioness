a46ea7df8fd98b2fd297ab036091789e1b0cc224
Douglas Dinarte <ddinarte@gmail.com>
2013-09-30 03:14:18 -0300

    DFD - Atualização do projeto para corrigir toda a nomenclatura dos métodos, adequando ao que foi definido anteriormente em sala de aula. Criado o restante do CRUD para as entidades que já foram implementadas no sistema, e ajustado também os métodos do projeto de Serviços.

---
+++ Trunk/Src/Fusiones.Main/Fusioness.Business/Usuario/IUsuarioBusiness.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Fusioness.Entities;
+
+namespace Fusioness.Business.Usuarios
+{
+    public interface IUsuarioBusiness
+    {
+        void DoSomething(Usuario usuario);
+        Usuario ValidarLogonUsuario(Usuario usuario);
+        Usuario InserirUsuario(Usuario usuario);
+        Usuario AlterarUsuario(Usuario usuario);
+        void RemoverUsuario(Usuario usuario);
+        Usuario ObterUsuarioPorId(Usuario usuario);
+        List<Usuario> ListarUsuarios();
+        Contato InserirContato(Contato contato);
+        List<Usuario> ListarContatosPorUsuario(Usuario usuario);
+    }
+}
---
+++ Trunk/Src/Fusiones.Main/Fusioness.Business/Usuario/UsuariosBusiness.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fusioness.Business.Util;
+using Fusioness.Data;
+using Fusioness.Data.Contracts;
+using Fusioness.Data.Repositories;
+using Fusioness.Entities;
+
+namespace Fusioness.Business.Usuarios
+{
+    public class UsuariosBusiness : IUsuarioBusiness
+    {
+        #region Properties
+
+        private readonly string _ConnectionString;
+
+        #endregion
+
+        #region Constructor
+
+        public UsuariosBusiness()
+        {
+            _ConnectionString = ConnectionBuilder.GetConnection();
+        }
+
+        public UsuariosBusiness(string connectionString)
+        {
+            _ConnectionString = connectionString;
+        }
+
+        #endregion
+
+        #region Methods
+        
+        public Usuario InserirUsuario(Usuario usuario)
+        {
+            try
+            {
+                using (IUnityOfWork uow = new EFUnityOfWork(_ConnectionString))
+                {
+                    IRepository<Usuario> repo = new UsuarioRepository(uow);
+                    usuario = repo.Insert(usuario);
+                    uow.Commit();
+                }
+                return usuario;
+            }
+            catch (Exception)
+            {
+                return default(Usuario);
+            }
+        }
+
+        public Usuario AlterarUsuario(Usuario usuario)
+        {
+            try
+            {
+                using (IUnityOfWork uow = new EFUnityOfWork(_ConnectionString))
+                {
+                    IRepository<Usuario> repo = new UsuarioRepository(uow);
+                    usuario = repo.Update(usuario);
+                    uow.Commit();
+                }
+                return usuario;
+            }
+            catch (Exception)
+            {
+                return default(Usuario);
+            }
+        }
+
+        public void RemoverUsuario(Usuario usuario)
+        {
+            try
+            {
+                using (IUnityOfWork uow = new EFUnityOfWork(_ConnectionString))
+                {
+                    IRepository<Usuario> repo = new UsuarioRepository(uow);
+                    repo.Delete(usuario);
+                    uow.Commit();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public Usuario ObterUsuarioPorId(Usuario usuario)
+        {
+            using (IUnityOfWork uow = new EFUnityOfWork(_ConnectionString))
+            {
+                return new UsuarioRepository(uow).GetByKey(new Usuario { IdUsuario = usuario.IdUsuario });
+            }
+        }
+        public List<Usuario> ListarUsuarios()
+        {
+            try
+            {
+                using (IUnityOfWork uow = new EFUnityOfWork(_ConnectionString))
+                {
+                    IRepository<Usuario> repo = new UsuarioRepository(uow);
+                    return repo.GetAll().ToList();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public Usuario ValidarLogonUsuario(Usuario usuario)
+        {
+            if (usuario == null || string.IsNullOrWhiteSpace(usuario.Login) || string.IsNullOrWhiteSpace(usuario.Senha)) return null;
+
+            using (IUnityOfWork ouw = new EFUnityOfWork(_ConnectionString))
+            {
+                var retorno = new UsuarioRepository(ouw).GetWhere(u => u.Senha == usuario.Senha && u.Login == usuario.Login).FirstOrDefault();
+                return retorno;
+            }
+        }
+
+        #region Contato
+        public Contato InserirContato(Contato contato)
+        {
+            try
+            {
+                using (IUnityOfWork uow = new EFUnityOfWork(_ConnectionString))
+                {
+                    IRepository<Contato> repo = new ContatoRepository(uow);
+                    contato = repo.Insert(contato);
+                    uow.Commit();
+                }
+                return contato;
+            }
+            catch (Exception)
+            {
+                return default(Contato);
+            }
+        }
+        public List<Usuario> ListarContatosPorUsuario(Usuario usuario)
+        {
+            try
+            {
+                using (IUnityOfWork uow = new EFUnityOfWork(_ConnectionString))
+                {
+                    IRepository<Usuario> repo = new UsuarioRepository(uow);
+                    repo.GetWhere(c => c.IdUsuario != usuario.IdUsuario);
+                    return repo.GetAll().ToList();
+                }
+            }
+            catch (Exception)
+            {
+                return new List<Usuario>();
+            }
+        }
+
+        #endregion
+
+        #endregion
+        public void DoSomething(Usuario usuario)
+        {
+            using (IUnityOfWork uow = new EFUnityOfWork(_ConnectionString))
+            {
+                IRepository<Usuario> repo = new UsuarioRepository(uow);
+                repo.Insert(usuario);
+                uow.Commit();
+            }
+        }
+    }
+}
