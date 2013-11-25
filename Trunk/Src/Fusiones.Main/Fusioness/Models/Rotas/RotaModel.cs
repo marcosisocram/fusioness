de42c1cd6b57d44bfbebb362e0a8d5f016027823
Douglas Dinarte <ddinarte@gmail.com>
2013-11-25 20:18:37 -0300

    Atualização devido a bugs.

--- Trunk/Src/Fusiones.Main/Fusioness/Models/Rotas/RotaModel.cs
+++ Trunk/Src/Fusiones.Main/Fusioness/Models/Rotas/RotaModel.cs
@@ -40,76 +40,10 @@ namespace Fusioness.Models.Rotas
             TiposDeQualidade = Servico.ListarQualidadesRota();
         }
         
-        public string TempoMedio(int idRota, Usuario usuario)
+        public double TempoMedio(int idRota)
         {
-            List<double> ltTotalMinutos = new List<double>();
-            string lblTempoMedio = "Tempo Médio: ";
-
             MainService Servico = new MainService();
-
-            Rota[] rotasSubsequentes = Servico.ListarRotasRealizadasPorRotaOriginal(new Rota { IdRota = idRota });
-            Evento[] eventos = Servico.ListarEventosPorUsuario(usuario);
-            EventoUsuario[] eventosUsuario = Servico.ListarEventoUsuario(usuario);
-            
-            
-            if (rotasSubsequentes.Length == 0 || eventos.Length == 0 || eventosUsuario.Length == 0)
-            {
-                return "Sem Tempo Médio";
-            }
-
-            List<Evento> ltEventos = eventos.Where(evnt => rotasSubsequentes.Any(rts => evnt.IdRota == rts.IdRota)).ToList();            
-            
-            List<EventoUsuario> ltEventosUsuario = eventosUsuario.Where(e => ltEventos.Any(ev => e.IdEvento == ev.IdEvento)).ToList();
-
-            if (ltEventosUsuario != null && ltEventosUsuario.Count > 0)
-            {
-                DateTime dataAtual = DateTime.Now;
-                foreach (var item in ltEventosUsuario)
-                {
-                    DateTime dateInicial;
-                    int horas = 0;
-                    int Minutos = 0;
-                    double TotalMinutos = 0;
-
-                    if (item.DataInicial != null)
-                    {
-                        dateInicial = Convert.ToDateTime(item.DataInicial);
-                        if (item.DataFinal != null)
-                        {
-                            DateTime dataFinal = Convert.ToDateTime(item.DataFinal);
-                            horas = (dataFinal - dateInicial).Hours;
-                            Minutos = (dataFinal - dateInicial).Minutes;
-                            TotalMinutos = (dataFinal - dateInicial).TotalMinutes;
-                        }
-                        else
-                        {
-                            horas = (dataAtual - dateInicial).Hours;
-                            Minutos = (dataAtual - dateInicial).Minutes;
-                            TotalMinutos = (dataAtual - dateInicial).TotalMinutes;
-                        }
-                    }
-
-                    ltTotalMinutos.Add(TotalMinutos);
-
-                }
-
-
-                if (ltTotalMinutos.Count > 0)
-                {
-                    double tempoMedio = (ltTotalMinutos.Sum(s => s)) / ltTotalMinutos.Count;
-                    lblTempoMedio += ""
-                        + TimeSpan.FromMinutes(tempoMedio).Hours.ToString() + ":"
-                        + TimeSpan.FromMinutes(tempoMedio).Minutes.ToString() + "h";
-                }
-                else
-                {
-                    lblTempoMedio += "0h";
-                }
-
-            }
-            else { lblTempoMedio = "Sem Tempo Médio"; }
-            
-            return lblTempoMedio;
+            return Servico.ConsultarDuracaoRota(idRota);
         }
      
     }
